b2882d127fb5389ea76bfee537856523fa671394
魔人老爸 <kmlixh@foxmail.com>
2016-04-29 10:57:02 +0800

    完成queryLink的基本开发。

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
 ### Bladea是一个Android敏捷开发框架：
 
 目前最主要的功能是用来处理Sqlite数据操作的POJO化：
-		
-    	即使用POJO来作为数据库操作的基本元素，不再过度关注数据库结构。让用户从繁杂的数据操作中解放出来！放个例子出来：
+
+     即使用POJO来作为数据库操作的基本元素，不再过度关注数据库结构。让用户从繁杂的数据操作中解放出来！放个例子出来：
         
 
 ```java
-Dao dao=Dao.getInstance();
-dao.query(UserInfo.class);
+Dao dao=Dao.getInstance(context);
+List<UserInfo> userList= dao.query(UserInfo.class);
 ```
--- bladeaLib/src/androidTest/java/com/janyee/bladea/Dao/DaoTest.java
+++ bladeaLib/src/androidTest/java/com/janyee/bladea/Dao/DaoTest.java
@@ -31,26 +31,7 @@ public class DaoTest extends AndroidTestCase {
 
     @Test
     public void testQuery1() throws Exception {
-        String id=UUID.randomUUID().toString().replace("-","");
-        Dao dao=Dao.getInstance(getContext());
-        Sort sort=new Sort();
-        sort.setSort_id(id);
-        sort.setSort_name("测试分类");
-        dao.save(sort);
-        List<Video> videoList=new ArrayList<>();
-        for(int i=0;i<100;i++){
-            Video video=new Video();
-            video.setId(i);
-            video.setSortId(id);
-            video.setTestInfo("sdfafasdf"+i);
-            video.setVideo_length(12312);
-            video.setVideo_name("fasdf23sdfsdf"+i);
-            video.setVideoUrl("http://www.baidu.com/");
-            videoList.add(video);
-        }
-        int i=dao.save(videoList);
-        dao.queryLinks(sort);
-        assertEquals(100,i);
+
     }
 
 
